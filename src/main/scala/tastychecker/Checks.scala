41a6f60e1e5ea3e8fa65eb325a7922f75f21e753
Guillem Bartrina I Moreno <guillembartrina@gmail.com>
2023-03-28 17:12:22 +0200

    Add first version of LSPCalculated

--- src/main/scala/tastychecker/Checks.scala
+++ src/main/scala/tastychecker/Checks.scala
@@ -15,6 +15,7 @@ object Check:
   private val _checks: Map[String, Check] =
     List(
       LSP,
+      LSPCalculated,
       LSPStatements,
       TypeParamBounds
     ).map(x => (x.name, x)).toMap
@@ -105,9 +106,68 @@ object LSP extends Check with MethodsLSP:
         } ++ checkSubtype(tr, defn.NothingType).toList
       case _ => Nil
     ret.toList
-          p <- checkSubtype(e, from.declaredType)
+
+// -------------------------------------------------------
+
+// LSP: Check computed types
+object LSPCalculated extends Check with MethodsLSP:
+  override def check(tree: Tree)(using Context): List[NotSubtype] =
+    given Tree = tree
+    val ret = tree match
+      case tr @ Apply(fun, _) =>
+        fun match
+          case Select(newObj @ New(_), SignedName(nme.Constructor, _, _)) =>
+            None
+          case _ =>
+            checkSubtype(fun.tpe.widen.asInstanceOf[MethodType].resultType, tr.tpe)
+      case tr @ Typed(expr, tpt) =>
+        checkSubtype(expr, tr) ++ checkSubtype(tpt.toType, tr)
+      case tr @ NamedArg(_, arg) =>
+        checkSubtype(arg, tr)
+      case tr @ Block(_, expr) =>
+        checkSubtype(expr, tr)
+      case tr @ If(_, thenPart, elsePart) =>
+        checkSubtype(thenPart, tr) ++ checkSubtype(elsePart, tr)
+      case tr @ InlineIf(_, thenPart, elsePart) =>
+        checkSubtype(thenPart, tr) ++ checkSubtype(elsePart, tr)
+      case tr @ Lambda(meth, tpt) =>
+        checkSubtype(meth, tr) ++ {
+          for
+            t <- tpt
+            p <- checkSubtype(t.toType, tr)
           yield p
+        }
+      case tr @ Match(_, cases) =>
+        for
+          b <- cases.map(_.body)
+          p <- checkSubtype(b, tr)
+        yield p
+      case tr @ InlineMatch(_, cases) =>
+        for
+          b <- cases.map(_.body)
+          p <- checkSubtype(b, tr)
+        yield p
+      case tr @ SeqLiteral(elems, elemtpt) =>
+        val tpe = tr.tpe.asInstanceOf[AppliedType].args(0)
+        checkSubtype(elemtpt.toType, tpe) ++ {
+          for
+            e <- elems
+            p <- checkSubtype(e, tpe)
+          yield p
+        }
+      case tr @ Try(expr, cases, finalizer) =>
+        checkSubtype(expr, tr) ++ {
+          for
+            b <- cases.map(_.body)
+            p <- checkSubtype(b, tr)
+          yield p
+        }
+      case tr @ Inlined(expr, _, _) =>
+        checkSubtype(expr, tr)
+      case tr @ Literal(constant) =>
+        checkSubtype(constant.wideType, tr.tpe.widen)
       case _ => Nil
+    ret.toList
 
 // -------------------------------------------------------
 
