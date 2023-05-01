7caef138c124552e6f3e0b81813582637bc8fc59
Guillem Bartrina I Moreno <guillembartrina@gmail.com>
2023-05-01 23:29:22 +0200

    Upgrade tasty-query version to 0.7.7, reimplement/rename all checks (and problems) (WIP)

--- src/main/scala/tastychecker/Problems.scala
+++ src/main/scala/tastychecker/Problems.scala
@@ -7,8 +7,11 @@ abstract class Problem(val message: String, val tree: Tree):
   override def toString(): String = s"${getClass.getSimpleName}: $message\n# $tree"
 
 
-case class NotSubtype(val a: Type, val b: Type, override val tree: Tree)
-  extends Problem(s"[$a] is not subtype of [$b]", tree)
+case class NotConformsType(val a: Type, val b: Type, override val tree: Tree)
+  extends Problem(s"[$a] does not conform to [$b]", tree)
 
-case class NotConformsToBounds(val a: Type, val b: TypeBounds, override val tree: Tree)
+case class NotMatchesType(val a: Type, val b: Type, override val tree: Tree)
+  extends Problem(s"[$a] does not match [$b]", tree)
+
+case class NotConformsBounds(val a: Type, val b: TypeBounds, override val tree: Tree)
   extends Problem(s"[$a] does not conform to bounds [$b]", tree)
